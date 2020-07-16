89d2d3ad712b8e016e3afa5eaf48d57faeed6cfc
Andrew Sarratori <ansarrat@microsoft.com>
2020-07-16 14:44:02 -0700

    Cleanup sample

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp4")]
+[assembly: AssemblyTitle("WPFSingleBorderWindowNoChrome")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp4")]
+[assembly: AssemblyProduct("WPFSingleBorderWindowNoChrome")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
